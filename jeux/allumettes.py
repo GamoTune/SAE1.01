63ca2fc378bc7116ce89fc0548be5b5f7e7511cb
Arthur Labregere <arthur.labregere@gmail.com>
2024-10-23 16:16:56 +0200

    Allumettes + utils
    Commencement du jeu des allumettes.
    Création du dossier utils + utils.py

---
+++ jeux/allumettes.py
@@ -0,0 +1,15 @@
+def allumettes() -> None:
+    """
+    Cette fonction est la fonction principale du jeu des allumettes. Elle permet de jouer à ce jeu.
+
+    Args:
+        (None): Aucun argument n'est nécessaire pour cette fonction.
+
+    Returns:
+        (None): Cette fonction ne retourne rien.
+    """
+    
+    #Déclaration des variables à utilisé
+    
+    
+    pass
