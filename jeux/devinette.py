4c6423647e6ffa0eada52d82e051cc017b318da2
Ichillow <blougaming555@gmail.com>
2024-10-23 16:32:54 +0200

    Procédure Joueur
    Procédure pour le login des joueurs

--- jeux/devinette.py
+++ jeux/devinette.py
@@ -1,8 +1,2 @@
 def devinette() :
-    joueur1 : str 
-    joueur2 : str
-
-    joueur1=str(input("Saisir le prénom du premier joueur : "))
-    joueur2=str(input("Saisir le prénom du second joueur : "))
-    
     return
