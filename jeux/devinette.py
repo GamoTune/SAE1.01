611a98c51f785433cf5442abc6cfa56dcad3df3b
Ichillow <blougaming555@gmail.com>
2024-10-24 08:40:40 +0200

    Update devinette.py

--- jeux/devinette.py
+++ jeux/devinette.py
@@ -1,2 +1,15 @@
+from utils.utils import login_joueur
+
 def devinette() :
+    joueur1 : str
+    joueur2 : str
+    coup : int
+    victoire : int
+
+    print("/------------------------------\\")
+    joueur1, joueur2 = login_joueur()
+
+    
+
+
     return
